1a7daa1497ae2e3e41f00d6892d3804190cfa520
李文昊 <vincentlieie@gmail.com>
2017-09-28 00:04:35 +0800

    Create 1.cpp

---
+++ baidu/1.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+char a[100];
+char s[100];
+int main()
+{
+    while(cin >> s) {
+        int ii = 0;
+        int len = strlen(s);
+        char t = s[0];
+        int idx = 0;
+        while(idx < len) {
+            t = s[idx];
+            for(int i = idx; i < len; i++) {
+                if(s[i] > t ) {
+                    t = s[i];
+                    idx = i;
+                }
+            }
+            a[ii++] = t;
+            idx++;
+        }
+        a[ii] = '\0';
+        cout << a << endl;
+    }
+
+}
