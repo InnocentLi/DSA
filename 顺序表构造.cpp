27d0884dc957a8316fcd6ecad2cc2a807c6a262c
VincentLieie <vincentlieie@gmail.com>
2018-04-16 14:28:13 +0800

    Update and rename 网易某水题.cpp to 顺序表构造.cpp

---
+++ 顺序表构造.cpp
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Vector {
+    int size, length;
+    int *data;
+} Vector;
+
+void init(Vector *vector, int size) {
+    vector->size = size;
+    vector->length = 0;
+    vector->data = (int *)malloc(sizeof(int)*size);
+}
+
+void clear(Vector *vector) {
+    free(vector->data);
+    free(vector);
+}
+
+int main() {
+    Vector *a = (Vector *)malloc(sizeof(Vector));
+    init(a, 100);
+    clear(a);
+    return 0;
+}
